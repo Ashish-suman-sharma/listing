f4533012b155b0051c42be54a919632a50290e75
Ashish-suman-sharma <ashish9264suman@gmail.com>
2023-12-30 03:28:09 +0530

    connect to mongo db

--- api/index.js
+++ api/index.js
@@ -1,8 +1,17 @@
+import mongoose from 'mongoose';
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.log(err);
+});
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server listening on port 3000');
-}
-);
+    console.log('Listening on port 3000');
+});
